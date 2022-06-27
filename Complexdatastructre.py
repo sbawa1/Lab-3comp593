258ee8fbf3e892b92692ce95196c6a53fb46e52d
shallu <Shallu.Bawa@flemingcollege.ca>
2022-06-27 12:46:45 -0300

    Update Complexdatastructre.py

--- Complexdatastructre.py
+++ Complexdatastructre.py
@@ -36,42 +36,48 @@ def main():
     print_pizza_toppings(Personal_Info)
     print_movies_list(Personal_Info)
 
+    print_my_name(Personal_Info)
+    print_my_student_id(Personal_Info)
+    print_pizza_toppings(Personal_Info)
+    print_movies_list(Personal_Info)
+    add_pizza_toppings_to_info(Personal_Info,favourite_topping)
+
     pass
 
 
-def print_my_name(info):
-    name = f"My name is' , {info['full_name']}, but you call me miss Shallu"
+def print_my_name(Personal_Info):
+    name = f"My name is' , {Personal_Info['full_name']}, but you call me miss Shallu"
  
-def print_my_student_id(info):
-    sentence =f"My student ID id', {info['student_id']}"
+def print_my_student_id(Personal_Info):
+    sentence =f"My student ID id', {Personal_Info['student_id']}"
     print(sentence)
  
-def add_pizza_toppings_to_info(info, favourite_toppings):
-    info['pizza_toppings'].extend(favourite_toppings)
+def add_pizza_toppings_to_info(Personal_Info, favourite_toppings):
+    Personal_Info['pizza_toppings'].extend(favourite_toppings)
 
-    for i,p in enumerate(info['pizza_toppings']):
-        info['pizza_toppings'][i] = p.capitalize()
+    for i,p in enumerate(Personal_Info['pizza_toppings']):
+        Personal_Info['pizza_toppings'][i] = p.capitalize()
 
-        info['pizza_toppings'].sort
+        Personal_Info['pizza_toppings'].sort
 
-def print_pizza_toppings(info):
+def print_pizza_toppings(Personal_Info):
     #print the heading
-    heading = f"{info['full_name']}'s favourite pizza toppings are:"
+    heading = f"{Personal_Info['full_name']}'s favourite pizza toppings are:"
     print(heading)
     print('-' * len(heading))
 
    #print dash list of all pizza toppings
-    for p in info['pizza_toppings']:
+    for p in Personal_Info['pizza_toppings']:
        print(f"- {p}")
        print()
 
-def print_movies_list(info):
+def print_movies_list(Personal_Info):
 
     #my favourite movies-
-    print(f"{info['full_name'].split(' ')[1]} like to watch", end='')
-    for i,t in enumerate(info['movies']):
+    print(f"{Personal_Info['full_name'].split(' ')[1]} like to watch", end='')
+    for i,t in enumerate(Personal_Info['movies']):
         print(t['genre'], end='')
-        if i < len(info['movies'])-1:
+        if i < len(Personal_Info['movies'])-1:
            print(',', end='')
     print(',', end='\n\n')
 
